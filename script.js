2153cdfb2ba456325cd2aeaf828853883cf692aa
unknown <jawadalikhel18@gmail.com>
2021-12-03 08:43:43 -0600

    implementing the game logic

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("testing for this project");
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'correct number';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+const number = Math.trunc(Math.random() * 20) + 1;
+console.log(number);
+
+document.querySelector('.number').textContent = number;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //   console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  } else {
+  }
+});
