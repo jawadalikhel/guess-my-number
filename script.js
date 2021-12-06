4e0432d9dcaa32e74fec37160e4e2c8dd04cc5e7
unknown <jawadalikhel18@gmail.com>
2021-12-06 00:37:13 -0600

    refactoring code: the DRY principle

--- script.js
+++ script.js
@@ -8,51 +8,58 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+let messageQuery = document.querySelector('.message');
+let numberQuery = document.querySelector('.number');
+let scoreQuery = document.querySelector('.score');
+let guessQuery = document.querySelector('.guess');
+let bodyQuery = document.querySelector('body');
+let highScoreQuery = document.querySelector('.highscore');
+
 //// even listner for listening on click the .check btn
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+  const guess = Number(guessQuery.value);
   //// when ther's not input number from player
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number';
+    messageQuery.textContent = 'No Number';
     //// when player's guess is correct and wins
   } else if (guess === secretNumber) {
     //// changing the text content of .number to secret number
-    document.querySelector('.number').textContent = secretNumber;
+    numberQuery.textContent = secretNumber;
 
-    document.querySelector('.message').textContent = 'correct number';
+    messageQuery.textContent = 'correct number';
     //// change background color when guess is correct
-    document.querySelector('body').style.backgroundColor = '#60b347';
+    bodyQuery.style.backgroundColor = '#60b347';
     //// change width to 30rem
-    document.querySelector('.number').style.width = '30rem';
+    numberQuery.style.width = '30rem';
 
     //// keeping track of the highScore and adjusting it
     if (score > highScore) {
       highScore = score;
-      document.querySelector('.highscore').textContent = highScore;
+      highScoreQuery.textContent = highScore;
     }
     //// when input number is greater then secret number
   } else if (guess > secretNumber) {
     //// when score is greater then 1
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high';
+      messageQuery.textContent = 'Too high';
       score--;
-      document.querySelector('.score').textContent = score;
+      scoreQuery.textContent = score;
       //// when score is less then 1
     } else {
-      document.querySelector('.message').textContent = 'YOU LOST THE GAME';
-      document.querySelector('.score').textContent = 0;
+      messageQuery.textContent = 'YOU LOST THE GAME';
+      scoreQuery.textContent = 0;
     }
     //// when input number is less then secret number
   } else if (guess < secretNumber) {
     //// when score is greater then 1
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low';
+      messageQuery.textContent = 'Too low';
       score--;
-      document.querySelector('.score').textContent = score;
+      scoreQuery.textContent = score;
       //// when score is less then 1
     } else {
-      document.querySelector('.message').textContent = 'YOU LOST THE GAME';
-      document.querySelector('.score').textContent = 0;
+      messageQuery.textContent = 'YOU LOST THE GAME';
+      scoreQuery.textContent = 0;
     }
   }
 });
@@ -62,17 +69,16 @@ document.querySelector('.again').addEventListener('click', function () {
 
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  messageQuery.textContent = 'Start guessing...';
 
   //   console.log(secretNumber, '<---- new');
-  document.querySelector('.score').textContent = score;
+  scoreQuery.textContent = score;
 
-  document.querySelector('.number').textContent = '?';
+  numberQuery.textContent = '?';
 
-  document.querySelector('.guess').value = '';
-
+  guessQuery.value = '';
   //// change background color when guess is correct
-  document.querySelector('body').style.backgroundColor = '#222';
+  bodyQuery.style.backgroundColor = '#222';
   //// change width to 30rem
-  document.querySelector('.number').style.width = '15rem';
+  numberQuery.style.width = '15rem';
 });
